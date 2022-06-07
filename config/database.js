bbe0a297200462751dfdf807452774549f003a3a
Kewaun Cain <kewaunscain@gmail.com>
2022-06-07 01:27:10 -0400

    first commit for Demo Day

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://socialApp:eBdmAHAqUjeDC3h1@cluster0.kmctg.mongodb.net/?retryWrites=true&w=majority', 
+    'dbName': 'DEMODAY'
+};
