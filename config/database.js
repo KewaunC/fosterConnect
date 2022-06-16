8619ed1afd89378dc4c62a5ec1f0d3a441f201d6
Kewaun <kewaunscain@gmail.com>
2022-06-16 17:46:47 -0400

    Delete database.js

--- config/database.js
+++
@@ -1,6 +0,0 @@
-// config/database.js
-module.exports = {
-    url: "mongodb+srv://socialApp:cF8y14KiwqWZBXDq@cluster0.kmctg.mongodb.net/demoDay?retryWrites=true&w=majority",    
-    dbName: "demoDay"
-  };
-    
