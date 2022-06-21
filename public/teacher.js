7459eded2f36c4a4bf11c7bba0fd6642c34d5a14
Kewaun Cain <kewaunscain@gmail.com>
2022-06-21 19:03:07 -0400

    added CSS

--- public/teacher.js
+++ public/teacher.js
@@ -16,3 +16,19 @@ Array.from(button).forEach(function(element) {
     })
   });
 });
+
+const editButton = this.parentNode.parentNode
+    console.log(editButton)
+    editButton.setAttribute('hidden', 'true');
+    editButton.nextElementSibling.removeAttribute('hidden', 'true');
+    console.log(editButton.nextElementSibling)
+    const noteId = this.parentNode.parentNode.parentNode.parentNode.getAttribute('data-noteId')
+    console.log(noteId)
+
+    editButton.nextElementSibling.addEventListener('click', function () {
+      let note = {
+        title: titleInput.value,
+        msg: messageInput.value
+      }
+      editNote(noteId, note)
+    })
